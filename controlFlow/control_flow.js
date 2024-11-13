1536235d29271ee5c9933df3d3cc01facae29e75
issverg <issverg@gmail.com>
2024-11-13 12:11:50 +0200

    second commit

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -4,6 +4,7 @@ let isLoggedIn = true;
 let userMessage;
 let userType = "subscriber";
 let userCategory;
+let isAuthenticated = true;
 
 if (userRole === "admin") {
     accessLevel = "Full access granted";
@@ -37,6 +38,9 @@ switch (userType) {
         userCategory = "Unknown";
 }
 
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+
 console.log("Access Level:", accessLevel);
 console.log("User Message:", userMessage);
 console.log("User Category:", userCategory);
+console.log("Authentication Status:", authenticationStatus);
