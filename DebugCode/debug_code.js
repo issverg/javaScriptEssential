18b8fe0941f759392e67c95030b3e54d02f374fe
issverg <issverg@gmail.com>
2024-11-16 23:47:27 +0200

    debug modified

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -2,22 +2,55 @@
 function performOperation() {
 	let num1 = parseInt(document.getElementById('input1').value);
 	let num2 = parseInt(document.getElementById('input2').value);
+	let results = [];
 	
 	if (!isNaN(num1) && !isNaN(num2)) {
-		let result = multiply(num1, num2);
-		displayResult(result);
+		results.push(add(num1, num2));
+		results.push(sub(num1, num2));
+		results.push(mul(num1, num2));
+		results.push(dev(num1, num2));
+		displayResult(results);
 	} else {
 		displayResult('Please enter valid numbers');
 	}
 }
 
-function multiply(a, b) {
+function add(a, b) {
 	debugger;
+	console.log("addition");
+	return a + b;
+}
+
+function sub(a, b) {
+	debugger;
+	console.log("substraction");
+	return a - b;
+}
+
+function mul(a, b) {
+	debugger;
+	console.log("multiplication")
 	return a * b;
 }
 
-function displayResult(result) {
+function dev(a, b) {
+	debugger;
+	console.log("devision");
+	
+	if(b === 0) {
+		alert("deviding by 0!");
+		return 0
+	}
+	
+	return a / b;
+}
+
+function displayResult(results) {
 	const resultElement = document.getElementById('result');
-	resultElement.textContent = `The result is: ${result}`;
+	resultElement.textContent = `The results are: 
+									addition: ${results[0]}
+									substraction: ${results[1]}
+									multiplication: ${results[2]}
+									devision: ${results[3]}`;
 }
         
