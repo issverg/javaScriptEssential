a7d495205797b54b81de0a3cfd1a980a7f0a23b6
issverg <issverg@gmail.com>
2024-11-18 19:24:16 +0200

    adding fetch

---
+++ weatherReport/weather_report.js
@@ -0,0 +1,24 @@
+const weatherInfo = document.getElementById('weatherInfo');
+const weatherForm = document.getElementById('weatherForm');
+
+weatherForm.addEventListener('submit', showweatherDetails);
+
+function showweatherDetails(event) {
+    event.preventDefault();
+	
+	const city = document.getElementById('city').value;
+	const apiKey = '39e96540f194465a33e9e489d109c8c8';
+	const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+	
+	fetch(apiUrl)
+		.then(res => res.json())
+		.then(data => {
+			weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+									  <p>Temperature: ${data.main.temp} &#8451;</p>
+									  <p>Weather: ${data.weather[0].description}</p>`;})
+		.catch(error => {
+			console.error('Error fetching weather:', error);
+			const weatherInfo = document.getElementById('weatherInfo');
+			weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+		});
+}
